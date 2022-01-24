2e46c3dc1262d7faa407bc32108a859fefd91d15
KhannaDeepika <96528030+KhannaDeepika@users.noreply.github.com>
2022-01-24 19:18:34 +0530

    Update run_kubernetes.sh

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -1,18 +1,19 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 
 # This tags and uploads an image to Docker Hub
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+ dockerpath=deepikakhannarbs/udacityproject4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+minikube kubectl -- create deployment udacity --image=$dockerpath:latest
 
 # Step 3:
 # List kubernetes pods
+minikube kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+minikube kubectl port-forward deployment/udacity 8000:80
