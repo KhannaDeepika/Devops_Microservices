98438061764f182de62a42b7e938f51124f1be7a
KhannaDeepika <96528030+KhannaDeepika@users.noreply.github.com>
2022-01-24 17:43:39 +0530

    Update upload_docker.sh

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+ dockerpath=deepikakhannarbs/udacityproject4
 
 # Step 2:  
 # Authenticate & tag
+docker login --username=deepikakhannarbs
+docker tag udacityproject $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
