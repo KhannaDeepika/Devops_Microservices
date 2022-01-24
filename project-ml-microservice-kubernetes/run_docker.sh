cc6d2c2352290690d13985a10d1192bf27b3fcc4
KhannaDeepika <96528030+KhannaDeepika@users.noreply.github.com>
2022-01-24 14:22:24 +0530

    Update run_docker.sh

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -3,10 +3,12 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-# Build image and add a descriptive tag
+docker build . -t udacityproject
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -it -p 8000:80  udacityproject
